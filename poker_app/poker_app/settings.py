71c2d6288878a566a21459a002a4a838aec97772
Akshay <akshay.sharma09695@gmail.com>
2020-04-17 22:48:37 +0530

    Add socket

--- poker_app/poker_app/settings.py
+++ poker_app/poker_app/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
+    'chat',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+ASGI_APPLICATION = "poker_app.routing.application"
